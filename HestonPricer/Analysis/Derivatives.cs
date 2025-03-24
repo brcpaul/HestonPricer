20d57deb52c7d307b204f727120b4ed58af07e2c
Paul BOURCEREAU <paul.bourcereau@edu.devinci.fr>
2025-03-24 22:01:51 +0100

    Updates

---
+++ HestonPricer/Analysis/Derivatives.cs
@@ -0,0 +1,72 @@
+using HestonPricer.Models;
+
+public class Derivatives
+{
+    private PricerBase pricer;
+
+    public Derivatives(PricerBase pricer)
+    {
+        this.pricer = pricer;
+    }
+
+    public double FirstOrderDerivative(string variable, double h = 0.0001)
+    {
+        double originalValue = GetVariableValue(variable);
+        SetVariableValue(variable, originalValue + h);
+        double valuePlusH = pricer.Price(); // Assuming Payoff method can handle null for simplicity
+        SetVariableValue(variable, originalValue - h);
+        double valueMinusH = pricer.Price();
+        SetVariableValue(variable, originalValue);
+        return (valuePlusH - valueMinusH) / (2 * h);
+    }
+
+    public double SecondOrderDerivative(string variable , double h = 0.0001)
+    {
+        double originalValue = GetVariableValue(variable);
+        SetVariableValue(variable, originalValue + h);
+        double valuePlusH = pricer.Price();
+        SetVariableValue(variable, originalValue - h);
+        double valueMinusH = pricer.Price();
+        SetVariableValue(variable, originalValue);
+        double valueOriginal = pricer.Price();
+        return (valuePlusH - 2 * valueOriginal - valueMinusH) / (h * h);
+    }
+
+    private double GetVariableValue(string variable)
+    {
+        switch (variable)
+        {
+            case "SpotPrice":
+                return pricer.Option.SpotPrice;
+            case "Strike":
+                return pricer.Option.Strike;
+            case "Maturity":
+                return pricer.Option.Maturity;
+            case "RiskFreeRate":
+                return pricer.Option.RiskFreeRate;
+            default:
+                throw new ArgumentException("Invalid variable name");
+        }
+    }
+
+    private void SetVariableValue(string variable, double value)
+    {
+        switch (variable)
+        {
+            case "SpotPrice":
+                pricer.Option.SpotPrice = value;
+                break;
+            case "Strike":
+                pricer.Option.Strike = value;
+                break;
+            case "Maturity":
+                pricer.Option.Maturity = value;
+                break;
+            case "RiskFreeRate":
+                pricer.Option.RiskFreeRate = value;
+                break;
+            default:
+                throw new ArgumentException("Invalid variable name");
+        }
+    }
+}
--- HestonPricer/Program.cs
+++ HestonPricer/Program.cs
@@ -1,31 +1,35 @@
 ﻿
 using ExcelDna.Integration;
+using HestonPricer.Models;
 
-double r = 0.03;      // Taux sans risque
+// double r = 0.03;      // Taux sans risque
 
-Console.WriteLine("test");
-public static class MyFunctions
-{
-    [ExcelFunction(Description = "My first .NET function")]
-    public static string SayHello(string name)
-    {
-        return "Hello " + name;
-    }
-}
+// public static class MyFunctions
+// {
+//     [ExcelFunction(Description = "My first .NET function")]
+//     public static string SayHello(string name)
+//     {
+//         return "Hello " + name;
+//     }
+// }
 
 
 // // Initialisation des paramètres
-// double q = 0.00;      // Rendement du dividende
-// double S0 = 100.0;    // Prix initial
-// double K = 100.0;     // Prix d'exercice
-// double T = 1.0;       // Maturité (en années)
-// double kappa = 1.5768;   // Taux de retour à la moyenne
-// double theta = 0.0398;  // Variance à long terme
-// double V0 = 0.1;     // Variance initiale
-// double sigma = 0.3;   // Volatilité de la volatilité
-// double rho = -0.5711;    // Corrélation
-// double lambda = 0.575; // Risk premium of volatiliy 
+double q = 0.00;      // Rendement du dividende
+double S0 = 100.0;    // Prix initial
+double K = 100.0;     // Prix d'exercice
+double T = 1.0;       // Maturité (en années)
+double kappa = 1.5768;   // Taux de retour à la moyenne
+double theta = 0.0398;  // Variance à long terme
+double V0 = 0.1;     // Variance initiale
+double sigma = 0.3;   // Volatilité de la volatilité
+double rho = -0.5711;    // Corrélation
+double lambda = 0.575; // Risk premium of volatiliy 
 
+
+EuropeanOption option = new EuropeanOption();
+
+// BlackScholesPricer pricer = new BlackScholesPricer();
 
 // HestonClosedFormPricer pricer = new HestonClosedFormPricer(r, q, S0, K, T, kappa, theta, lambda, V0, sigma, rho);
 // double price = pricer.RectangleMethodPrice();
